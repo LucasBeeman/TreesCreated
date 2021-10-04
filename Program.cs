e153ecd429efc3f861a4316d68ba9cf1ea66374b
Lucas Beeman <lucasbeeman12@gmail.com>
2021-10-04 09:29:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest f1 = new Forest("Amazon");
+            Console.WriteLine(f1.Trees);
+            f1.Grow();
+            Console.WriteLine(f1.Trees);
+        }
+    }
+}
